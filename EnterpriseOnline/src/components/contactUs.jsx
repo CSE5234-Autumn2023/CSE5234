c5643f705936ebb024af2bb4d5c2ec6c4038f77c
Jason Weible <weible.jason@gmail.com>
2023-10-08 20:56:13 -0400

    update contactUs.jsx

--- EnterpriseOnline/src/components/contactUs.jsx
+++ EnterpriseOnline/src/components/contactUs.jsx
@@ -3,8 +3,11 @@ import "./styles/contactUs.css";
 
 function ContactUs() {
     return (
-        <div className="contactUs">
-            <h1>Enterprise Online</h1>
+        <div>
+            <h1 className="contactUs">Enterprise Online</h1>
+            <p className="center">We'd love to hear from you!</p>
+            <p className="center">Call us at 123-456-7890</p>
+            <p className="center">Email us at help@EnterpriseOnline.com</p>
         </div>
     )
 }
