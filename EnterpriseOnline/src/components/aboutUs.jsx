bbd1e5b85fd00b476ea224dd8b48451be5957471
paulkoenig7 <64798109+paulkoenig7@users.noreply.github.com>
2023-11-27 20:09:25 -0500

    Style about us page to match home page

--- EnterpriseOnline/src/components/aboutUs.jsx
+++ EnterpriseOnline/src/components/aboutUs.jsx
@@ -4,34 +4,28 @@ import './styles/aboutUs.css';
 function AboutUs() {
     return (
         <div>
-            <div className="center">
-                <h1>Welcome to Enterprise Online</h1>
-                <p>We specialize in selling technology and homegoods products that are the best you can buy!</p>
-                <p>We offer an array of electronics like cameras and watches, as well as the famous iced milk.</p>
-
-                <h2>The Team</h2>
+            <div className="center about-us-title">
+                <h1>The Team</h1>
             </div>
 
             <div className="offset-md-1 col-md-10 team-intros">
                 <div className="row">
-                    <div className="col">
+                    <div className="col team-mem-bio">
                         <div className="center">
-                            <img src={require("../data/AboutUsPhotos/Nick.jpg")} alt="Nick Pic" height="240" width="200" />
-                            <h4>Nicolas Ashbaugh</h4>
-                            <p>CEO</p>
+                            <img className="team-mem-img" src={require("../data/AboutUsPhotos/Nick.jpg")} alt="Nick Pic" height="240" width="200" />
+                            <h4>Nicolas Ashbaugh - CEO</h4>
                         </div>
-                        <div className='center'>
+                        <div className='center team-mem-desc'>
                             <p>Nick is a Senior at OSU that studies Computer Science & Engineering</p>
                             <p>He brings 21 years of life experience to the team as well as some professional coding experience.</p>
                         </div>
                     </div>
-                    <div className="col">
+                    <div className="offset-md-1 col team-mem-bio">
                         <div className="center">
-                            <img src={require("../data/AboutUsPhotos/Paul.jpg")} alt="Paul Pic" height="240" width="200" />
-                            <h4>Paul Koenig</h4>
-                            <p>CIO</p>
+                            <img className="team-mem-img" src={require("../data/AboutUsPhotos/Paul.jpg")} alt="Paul Pic" height="240" width="200" />
+                            <h4>Paul Koenig - CIO</h4>
                         </div>
-                        <div className='center'>
+                        <div className='center team-mem-desc'>
                             <p>Paul is a Senior at OSU that studies Computer Science & Engineering</p>
                             <p>He brings a great spirit to the team and occasionally codes I guess.</p>
                         </div>
