15b8c41fc87c79e9c34cb1bfa8832e6dade2cb6e
Nicolas124816 <nicolas.ashbaugh@gmail.com>
2023-11-27 21:57:22 -0500

    Updated contact us page

--- EnterpriseOnline/src/components/contactUs.jsx
+++ EnterpriseOnline/src/components/contactUs.jsx
@@ -3,28 +3,40 @@ import "./styles/contactUs.css";
 
 function ContactUs() {
     return (
-        <div className="contact-us-background overlay">
-            <div className="overlay">
-                <h1 className="contact-us-title">Contact Us</h1>
-                <div className="offset-md-1 col-md-10 company-discussion">
-                    <div className="row">
-                        <div className="col">
-                            <div className="center">
-                                <h4>Phone</h4>
-                                <p>555-555-1234</p>
-                            </div>
+        <div>
+            <div className="center about-us-title">
+                <h1>Contact Us</h1>
+            </div>
+            <div className="offset-md-1 col-md-10 contact-option-title">
+                <div className="row">
+                    <div className="col mb-4 contact-option">
+                        <div className="center">
+                            <img className="team-mem-img" src={require("../data/ContactUsPhotos/Shaq_phone.jpg")} alt="Shaq's Shoe Phone" height="240" width="200" />
+                            <h4>Phone</h4>
                         </div>
-                        <div className="col">
-                            <div className="center">
-                                <h4>Email</h4>
-                                <p>help@EnterpriseOnline.com</p>
-                            </div>
+                        <div className='center team-mem-desc'>
+                            <p>You can contact us through our 24/7 hotline.</p>
+                            <p>1-800-555-1234</p>
                         </div>
-                        <div className="col">
-                            <div className="center">
-                                <h4>Address</h4>
-                                <p>1234 High Street, Columbus Ohio</p>
-                            </div>
+                    </div>
+                    <div className="col mb-4 contact-option">
+                        <div className="center">
+                            <img className="team-mem-img" src={require("../data/ContactUsPhotos/Shooting_basketball.jpg")} alt="Shaq's Shoe Phone" height="240" width="200" />
+                            <h4>Email</h4>
+                        </div>
+                        <div className='center team-mem-desc'>
+                            <p>Feel free to shoot us an email.</p>
+                            <p>help@BasketballRUs.com</p>
+                        </div>
+                    </div>
+                    <div className="col mb-4 contact-option">
+                        <div className="center">
+                            <img className="team-mem-img" src={require("../data/ContactUsPhotos/Basketball_wall.jpg")} alt="Shaq's Shoe Phone" height="240" width="200" />
+                            <h4>Address</h4>
+                        </div>
+                        <div className='center team-mem-desc'>
+                            <p>Or stop by our warehouse.</p>
+                            <p>1234 High Street, Columbus Ohio</p>
                         </div>
                     </div>
                 </div>
